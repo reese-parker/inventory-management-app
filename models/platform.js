e37d102c752eb83c28f519970de8112084f77ae8
Reese Parker <parker.rhys@outlook.com>
2022-05-16 15:01:58 -0400

    Create platform schema

---
+++ models/platform.js
@@ -0,0 +1,14 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var PlatformSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+});
+
+PlatformSchema.virtual("url").get(function () {
+  return "/platform/" + this._id;
+});
+
+module.exports = mongoose.model("Platform", PlatformSchema);
