ca15e1b271d38f9fd602178da6f864a98ce1aeb7
Reese Parker <parker.rhys@outlook.com>
2022-05-17 15:14:08 -0400

    Add GET and POST request for update platform

--- controllers/platformController.js
+++ controllers/platformController.js
@@ -4,7 +4,6 @@ var Game = require("../models/game");
 var async = require("async");
 const { body } = require("express-validator");
 
-
 // Read all platforms
 
 exports.platform_list = function (req, res, next) {
@@ -43,8 +42,8 @@ exports.platform_detail = function (req, res, next) {
 // Create platform GET request
 
 exports.platform_create_get = function (req, res, next) {
-  res.render("platform_form")
-}
+  res.render("platform_form", { title: "Add platform", platform: undefined });
+};
 
 // Create platform POST request
 
@@ -54,14 +53,50 @@ exports.platform_create_post = [
   function (req, res, next) {
     var platform = new Platform({
       name: req.body.name,
-      description: req.body.description
-    })
+      description: req.body.description,
+    });
 
-    platform.save(function(err) {
-      if(err) {
-        return next(err)
+    platform.save(function (err) {
+      if (err) {
+        return next(err);
       }
-      res.redirect(platform.url)
-    })
-  }
-]
+      res.redirect(platform.url);
+    });
+  },
+];
+
+// Update platform GET request
+
+exports.platform_update_get = async function (req, res, next) {
+  var platform_details = await Platform.findById(req.params.id);
+  res.render("platform_form", {
+    title: "Update platform",
+    platform: platform_details,
+  });
+};
+
+// Update platform POST request
+
+exports.platform_update_post = [
+  body("name").trim(),
+  body("description").trim(),
+  function (req, res, next) {
+    var updatedPlatform = new Platform({
+      name: req.body.name,
+      description: req.body.description,
+      _id: req.params.id,
+    });
+
+    Platform.findByIdAndUpdate(
+      req.params.id,
+      updatedPlatform,
+      {},
+      function (err, results) {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(results.url);
+      }
+    );
+  },
+];
--- routes/platforms.js
+++ routes/platforms.js
@@ -10,6 +10,10 @@ router.get("/create", platform_controller.platform_create_get);
 
 router.post("/create", platform_controller.platform_create_post);
 
+router.get("/:id/update", platform_controller.platform_update_get)
+
+router.post("/:id/update", platform_controller.platform_update_post)
+
 router.get("/:id", platform_controller.platform_detail);
 
 module.exports = router;
