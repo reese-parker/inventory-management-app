d9c180db971c989d0cb4b23a65a4b9d05d79a560
Reese Parker <parker.rhys@outlook.com>
2022-05-17 13:42:13 -0400

    Add POST request for create game form

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,5 +1,10 @@
 const { response } = require("../app");
 var Game = require("../models/game");
+var Platform = require("../models/platform");
+var async = require("async");
+const { body } = require("express-validator");
+
+// Read all games
 
 exports.game_list = function (req, res, next) {
   Game.find({}, "name platform")
@@ -12,6 +17,8 @@ exports.game_list = function (req, res, next) {
     });
 };
 
+// Read game
+
 exports.game_detail = function (req, res, next) {
   Game.findById(req.params.id)
     .populate("platform")
@@ -19,7 +26,42 @@ exports.game_detail = function (req, res, next) {
       if (err) {
         return next(err);
       }
-      console.log(results)
+      console.log(results);
       res.render("game_detail", { game: results });
     });
 };
+
+// Create game form GET request
+
+exports.game_create_get = function (req, res, next) {
+  Platform.find().exec(function (err, list_platforms) {
+    if (err) {
+      return next(err);
+    }
+    res.render("game_form", { platforms: list_platforms });
+  });
+};
+
+// Create game form POST request
+
+exports.game_create_post = [
+  body("name").trim(),
+  body("description").trim(),
+  function (req, res, next) {
+    var game = new Game({
+      name: req.body.name,
+      platform: req.body.platform,
+      description: req.body.description,
+      release_date: req.body.release_date,
+      price: req.body.price,
+      stock: req.body.stock,
+    });
+
+    game.save(function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect(game.url);
+    });
+  },
+];
--- routes/games.js
+++ routes/games.js
@@ -6,6 +6,10 @@ var game_controller = require("../controllers/gameController");
 
 router.get("/", game_controller.game_list);
 
+router.get("/create", game_controller.game_create_get);
+
+router.post("/create", game_controller.game_create_post);
+
 router.get("/:id", game_controller.game_detail);
 
 module.exports = router;
